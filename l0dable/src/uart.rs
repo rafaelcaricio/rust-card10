e882db08455634a23b39ac839f20490cec3c2662
Kloenk <kloenk@kloenk.de>
2019-08-24 00:23:10 +0200

    add print! and println! macro to write to UART

--- l0dable/src/uart.rs
+++ l0dable/src/uart.rs
@@ -11,3 +11,20 @@ impl Write for Uart {
         Ok(())
     }
 }
+
+#[macro_export]
+macro_rules! print {
+    ($($arg:tt)*) => ($crate::uart::_print(format_args!($($arg)*)));
+}
+
+#[macro_export]
+macro_rules! println {
+    () => ($crate::print!("\r"));
+    ($($arg:tt)*) => ($crate::print!("{}\r", format_args!($($arg)*)));
+}
+
+#[doc(hidden)]
+pub fn _print(args: core::fmt::Arguments) {
+    use core::fmt::Write;
+    crate::UART.write_fmt(args);
+}
