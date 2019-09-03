0482400782b651946aa0fd72b5b6054f9deb6999
Astro <astro@spaceboyz.net>
2019-09-04 01:13:28 +0200

    alloc: doc

--- card10-alloc/src/lib.rs
+++ card10-alloc/src/lib.rs
@@ -1,7 +1,28 @@
+//! Support for dynamically allocated memory
+//!
 //! Reproduces l0dable hardware.c's `_sbrk()`
 //!
 //! Unfortunately, we cannot link _sbrk()` directly because it
 //! references the unwieldy `errno`.
+//!
+//! ## Example
+//!
+//! ```rust
+//! #![no_std]
+//! #![no_main]
+//!
+//! extern crate alloc;
+//! use alloc::vec;
+//! use card10_l0dable::*;
+//!
+//! main!(main);
+//! fn main() {
+//!     // Pass stack headroom
+//!     card10_alloc::init(128 * 1024);
+//!     let mut xs = vec![];
+//!     xs.push(23);
+//! }
+//! ```
 #![no_std]
 #![feature(asm)]
 #![feature(alloc_error_handler)]
@@ -26,7 +47,10 @@ fn sp() -> usize {
     value
 }
 
-/// You must call this before using anything from `alloc`.
+/// Call this before using anything from `alloc`.
+///
+/// Consider the size of your stack-allocated variables for the
+/// `stack_headroom` parameter.
 ///
 /// Returns heap size
 pub fn init(stack_headroom: usize) -> usize {
